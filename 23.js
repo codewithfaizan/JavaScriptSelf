eacaba0c0e2e43e51c5df3e8ee33e7376013f243
codewithfaizan <faizanoic.com@gmail.com>
2022-07-26 00:04:24 +0530

    object

--- 23.js
+++ 23.js
@@ -1,11 +1,9 @@
-const person = {name:"faizan",
-age:"20",
-hobbies: ["football","basketbal"]};
+const person = {name:"faizan",age:"20",hobbies: ["football","basketball"]};
 // person = {name:'faizanoic'};
 // console.log(person); 
 person.gender = "male"
 
-console.log(person);
-console.log(typeof null)
+console.log(person.hobbies[1]);
+// console.log(typeof null)
 
 
